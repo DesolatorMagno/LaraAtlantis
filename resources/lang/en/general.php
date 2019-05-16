007a6875f0ce2252d7a975e7e817627f22f0c0b7
Reny Ramos <renyramosgarcia@gmail.com>
2019-05-16 17:43:52 -0400

    Translation for theme

---
+++ resources/lang/en/general.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'listings'            => 'Listings',
+    'listing'             => 'Listing',
+    'list'                => 'List',
+    'employees'           => 'Employees',
+    'employee'            => 'Employee',
+    'companies'           => 'Companies',
+    'company'             => 'Company',
+    'home'                => 'Home',
+    'back'                => 'Back',
+    'update'              => 'Update',
+    'submit'              => 'Submit',
+    'details'             => 'Details',
+    'edit'                => 'Edit',
+    'delete'              => 'Delete',
+    'creation'            => 'Creation',
+    'new'                 => 'New :Model',
+    'actions'             => 'Actions',
+    'delete'              => 'Delete',
+    'delete_confirmation' => 'Are you really sure?',
+    'dashboard'           => 'Dashboard',
+
+];
